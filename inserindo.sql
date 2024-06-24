90444b1bfd59ab9fd2dc65fb9f1cb90e21d3c50f
dudapascoall <142435751+dudapascoall@users.noreply.github.com>
2024-06-24 12:14:49 -0300

    Add files via upload

---
+++ inserindo.sql
@@ -0,0 +1,6 @@
+INSERT INTO `tbl_pessoas` (`nome`)  VALUES ('Alfredo');
+INSERT INTO `tbl_pessoas` (`nome`)  VALUES ('Bento');
+INSERT INTO `tbl_pessoas` (`nome`)  VALUES ('Charles');
+INSERT INTO `tbl_pessoas` (`nome`)  VALUES ('Dinelson');
+INSERT INTO `tbl_pessoas` (`nome`)  VALUES ('Eufrades');
+ INSERT INTO `tbl_pessoas` (`nome`)  VALUES ('Franklin');
